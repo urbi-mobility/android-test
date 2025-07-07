8b305e02e5df0b35e5ca5f12d96c84562ffcfadb
Domenico <dommy.g1985@gmail.com>
2025-07-07 13:06:17 +0200

    Added test button state

--- app/src/main/java/com/example/testapplication/TestViewModel.kt
+++ app/src/main/java/com/example/testapplication/TestViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.testapplication
 
 import androidx.lifecycle.ViewModel
+import com.example.testapplication.model.TestState
+import com.example.testapplication.model.getRandomState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class TestViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow<TestState>(TestState.StatoIniziale)
+    val uiState: StateFlow<TestState> = _uiState
 
+    fun updateToRandomState() {
+        _uiState.value = getRandomState()
+    }
 }
--- app/src/main/java/com/example/testapplication/model/TestState.kt
+++ app/src/main/java/com/example/testapplication/model/TestState.kt
@@ -6,3 +6,13 @@ sealed class TestState {
     data class StatoErrore(val errore: String) : TestState()
     data class StatoCompletato(val risultato: String) : TestState()
 }
+
+fun getRandomState(): TestState {
+    return when ((1..4).random()) {
+        1 -> TestState.StatoIniziale
+        2 -> TestState.StatoCaricamento("Caricamento in corso...")
+        3 -> TestState.StatoErrore("Errore di rete")
+        4 -> TestState.StatoCompletato("Operazione completata con successo")
+        else -> TestState.StatoIniziale
+    }
+}
